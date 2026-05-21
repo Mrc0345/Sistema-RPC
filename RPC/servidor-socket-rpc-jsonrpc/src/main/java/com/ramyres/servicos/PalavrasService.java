a3c5d6ed6fc5ade92b372000ce3ac5c0362cdfa0
Mrc0345 <marcusviniciusor09@gmail.com>
2026-05-21 17:26:30 -0300

    implementação de Palavras service

--- RPC/servidor-socket-rpc-jsonrpc/src/main/java/com/ramyres/servicos/PalavrasService.java
+++ RPC/servidor-socket-rpc-jsonrpc/src/main/java/com/ramyres/servicos/PalavrasService.java
@@ -2,21 +2,68 @@ package com.ramyres.servicos;
 
 public class PalavrasService implements ServiceInterface {
 
-    @Override
+     @Override
     public String[] Run(String[] params) {
-        // TODO Auto-generated method stub
-        // throw new UnsupportedOperationException("Unimplemented method 'Run'");
-        //[qtdLetras, qtdPalavras, qtdVogal, qtdVogalAcentuada, qtdConsoantes, qtdEspacos, qtdPalavrasPar, qtdPalavrasImpar, qtdDeLinhas ]
+        if (params.length != 1) {
+            throw new IllegalArgumentException("Parametros invalidos. Esperado: texto");
+        }
+
+        String texto = params[0];
+        int qtdLetras = 0;
+        int qtdVogal = 0;
+        int qtdVogalAcentuada = 0;
+        int qtdConsoantes = 0;
+        int qtdEspacos = 0;
+        int qtdPalavrasPar = 0;
+        int qtdPalavrasImpar = 0;
+
+        for (int i = 0; i < texto.length(); i++) {
+            char caractere = texto.charAt(i);
+
+            if (caractere == ' ') {
+                qtdEspacos++;
+            }
+
+            if (Character.isLetter(caractere)) {
+                qtdLetras++;
+                char letra = Character.toLowerCase(caractere);
+
+                if ("aeiou".indexOf(letra) >= 0) {
+                    qtdVogal++;
+                } else if ("áàâãéèêíìîóòôõúùû".indexOf(letra) >= 0) {
+                    qtdVogalAcentuada++;
+                } else {
+                    qtdConsoantes++;
+                }
+            }
+        }
+
+        String textoSemPontuacao = texto.replaceAll("[^\\p{L}\\s]", " ");
+        String[] palavras = textoSemPontuacao.trim().isEmpty()
+                ? new String[0]
+                : textoSemPontuacao.trim().split("\\s+");
+
+        for (String palavra : palavras) {
+            if (palavra.length() % 2 == 0) {
+                qtdPalavrasPar++;
+            } else {
+                qtdPalavrasImpar++;
+            }
+        }
+
+        int qtdPalavras = palavras.length;
+        int qtdDeLinhas = texto.isEmpty() ? 0 : texto.split("\\R", -1).length;
+
         return new String[]{
-            "38",
-            "10",
-            "18",
-            "1",
-            "19",
-            "9",
-            "4",
-            "6",
-            "1"
+            String.valueOf(qtdLetras),
+            String.valueOf(qtdPalavras),
+            String.valueOf(qtdVogal),
+            String.valueOf(qtdVogalAcentuada),
+            String.valueOf(qtdConsoantes),
+            String.valueOf(qtdEspacos),
+            String.valueOf(qtdPalavrasPar),
+            String.valueOf(qtdPalavrasImpar),
+            String.valueOf(qtdDeLinhas)
         };
     }
 }
